64df89b5c18625f05fb22b544172f771340bd697
Barun Das <123059240+barundas97@users.noreply.github.com>
2024-02-28 21:28:55 +0530

    Update script.js
    Re-new the code for AM/PM format

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const hrs = document.querySelector("#hrs")
-const min = document.querySelector("#mins")
-const sec = document.querySelector("#sec")
-const title = document.querySelector("#h2");
+const hrs = document.querySelector("#hrs");
+const min = document.querySelector("#mins");
+const sec = document.querySelector("#sec");
+const h2 = document.querySelector("#h2");
 
-setInterval(()=>{
+setInterval(() => {
     let time = new Date();
     
-    hrs.innerHTML = time.getHours() %12 || 12;
-    title.innerHTML = hrs >= 12 ? "PM":"AM";
+    let hours = time.getHours() % 12 || 12; // Convert hours to 12-hour format
+    let period = time.getHours() >= 12 ? "PM" : "AM"; // Determine AM or PM
+    
+    hrs.innerHTML = hours;
+    h2.innerHTML = period;
     min.innerHTML = time.getMinutes();
     sec.innerHTML = time.getSeconds();
-},1000);
+}, 1000);
